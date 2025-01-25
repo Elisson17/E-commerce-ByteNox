efc2cc6c060e62a0255b235550d83f415069f1fb
Elisson Silva <elissoncordeiro2004@gmail.com>
2025-01-25 07:11:59 -0300

    Adicionando logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,19 @@ import { FaSignOutAlt } from "react-icons/fa";
 import SignOutButton from "./SignOutButton";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import logo from "../../public/image/logo.png";
+import Link from "next/link";
 
 export default function Header() {
   const { data: session } = useSession();
   const router = useRouter();
 
   return (
-    <header className="fixed z-50 top-0 left-0 flex h-14 w-full items-center justify-between bg-white px-4">
-      <span className="text-black rubik font-bold text-3xl">ByteNox</span>
+    <header className="fixed z-50 top-0 left-0 flex h-18 w-full items-center justify-between bg-white px-4">
+      <Link href="/" className="w-40">
+        <Image src={logo} alt="Logo" width={160} height={160} />
+      </Link>
       {session?.user ? (
         <div className="flex items-center gap-4">
           <span className="text-gray-600 text-sm">
